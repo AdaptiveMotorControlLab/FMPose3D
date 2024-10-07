df4735c1edd17bfe6a1f1b2c7d03f57675a9e167
Ti <ti.wang@epfl.ch>
2024-10-07 17:13:36 +0200

    add debug mode

--- PrimatePose/coco_necessary/build_config.sh
+++ PrimatePose/coco_necessary/build_config.sh
@@ -1,9 +1,9 @@
 # proj_root=/home/ti_wang/Ti_workspace/PrimatePose/data/data/v8_1_coco
 # train_json=/mnt/tiwang/primate_data/primate_train_1.1.json
 proj_root=/mnt/tiwang/v8_coco
-file=pfm_val_v8
-# train_json="/mnt/tiwang/primate_data/splitted_val_datasets/${file}.json"
-train_json="/mnt/tiwang/primate_data/${file}.json"
+file=ap10k_val
+train_json="/mnt/tiwang/primate_data/splitted_val_datasets/${file}.json"
+# train_json="/mnt/tiwang/primate_data/${file}.json"
 model_arch="top_down_resnet_50"
 out_name="/home/ti_wang/Ti_workspace/PrimatePose/project/debug_${file}"
-python make_config.py $proj_root $out_name $model_arch --train_file $train_json --multi_animal
+python make_config.py $proj_root $out_name $model_arch --train_file $train_json --multi_animal --debug
