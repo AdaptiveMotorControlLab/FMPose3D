a286fc26f07a2b19e9d0227af5b06b0dc2e45aa2
ti <tiwang.cs@gmail.com>
2024-11-12 23:43:43 +0100

    update

--- PrimatePose/coco_necessary_split/train.sh
+++ PrimatePose/coco_necessary_split/train.sh
@@ -1,22 +1,19 @@
-# for amgm0
-# proj_root=/home/ti/projects/PrimatePose/ti_data/data/v8_coco
-# train_file=/mnt/tiwang/primate_data/primate_test_1.1.json
-# test_file=/mnt/tiwang/primate_data/primate_test_1.1.json
-
 # server 7
 data_path_prefix="/mnt/data/tiwang"
 # server amgm0
 # data_path_prefix="/media/data/ti/data"
 
-proj_root=${data_path_prefix}+"/v8_coco"
+proj_root=${data_path_prefix}"/v8_coco"
 
 debug=0
 gpu_id="1"
-file=chimpact
+dataset_file=oap
+file=oap_hrnet
+# file=chimpact_hrnet_onlyPose
 mode="train"
 # for splitted datasets
-train_file=${data_path_prefix}+/primate_data/splitted_train_datasets/${file}_train.json
-test_file=/${data_path_prefix}/primate_data/splitted_test_datasets/${file}_test.json
+train_file=${data_path_prefix}/primate_data/splitted_train_datasets/${dataset_file}_train.json
+test_file=${data_path_prefix}/primate_data/splitted_test_datasets/${dataset_file}_test.json
 # train_file=/mnt/tiwang/primate_data/splitted_val_datasets/${file}_sampled_500.json
 # test_file=/mnt/tiwang/primate_data/splitted_val_datasets/${file}_sampled_500.json
 
@@ -26,7 +23,7 @@ test_file=/${data_path_prefix}/primate_data/splitted_test_datasets/${file}_test.
 if [ "$debug" -eq 1 ]; then
     pytorch_config=/app/project/Debug/${file}_${mode}/train/pytorch_config.yaml
     echo "Debug mode is ON, using debug pytorch_config: $pytorch_config"
-    CUDA_VISIBLE_DEVICES=$gpu_id python train.py --debug $proj_root $pytorch_config --train_file $train_file --test_file $test_file --device cuda --gpus 0
+    CUDA_VISIBLE_DEVICES=$gpu_id python train.py --debug --project_root $proj_root --pytorch_config $pytorch_config --train_file $train_file --test_file $test_file --device cuda --gpus 0
 else
     pytorch_config=/app/project/split/${file}_${mode}/train/pytorch_config.yaml
     echo "Debug mode is OFF, using default pytorch_config: $pytorch_config"
