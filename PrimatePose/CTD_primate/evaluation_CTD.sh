2ab6745ca5cd596fa62b5acab35d5f0bbc7cf1a0
ti <tiwang.cs@gmail.com>
2025-05-06 21:04:02 +0200

    Add evaluation script for CTD model

---
+++ PrimatePose/CTD_primate/evaluation_CTD.sh
@@ -0,0 +1,6 @@
+# CTD training
+CUDA_VISIBLE_DEVICES=0 python CTD_primate.py \
+--config "./projects/riken_bandy-ti-2025-05-06/config.yaml" \
+--bu_shuffle 1 \
+--ctd_shuffle 2 \
+--evaluate_CTD True
