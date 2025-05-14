ef92ca1e995d4c6c9c89df83210a83635ed92534
ti <tiwang.cs@gmail.com>
2025-05-14 20:46:09 +0200

    optimize memory parameters and fix test JSON file path

--- PrimatePose/SuperAnimal/train_Quadruped.sh
+++ PrimatePose/SuperAnimal/train_Quadruped.sh
@@ -6,10 +6,14 @@ debug=0
 gpu_id="0"
 OOD_dataset_name=AP-10K
 
-file_name=SA_pose_rtmpose_s_OOD_${OOD_dataset_name}_20250514
+file_name=SAQ_pose_rtmpose_s_OOD_${OOD_dataset_name}_20250514
 
 train_pose=1
 train_detector=0
+
+# Memory optimization parameters
+batch_size=32  # Reduced from default 32
+dataloader_workers=4  # Reduced from default 16
 
 mode="train"
 
@@ -18,7 +22,7 @@ run_name="${file_name}"
 
 # train and test json files
 train_json="${data_root}/annotations/${mode}_IID_wo_${OOD_dataset_name}.json"
-test_json="${data_root}/annotations/${mode}_IID_wo_${OOD_dataset_name}.json"
+test_json="${data_root}/annotations/test_IID_wo_${OOD_dataset_name}.json"
 
 if [ "$debug" -eq 1 ]; then
     pytorch_config=${project_root}/experiments/Debugs/${file_name}/train/pytorch_config.yaml
@@ -28,7 +32,8 @@ if [ "$debug" -eq 1 ]; then
         $([ "$train_pose" -eq 1 ] && echo "--train-pose") \
         --project_root $data_root --pytorch_config $pytorch_config \
         --train_file $train_file --test_file $test_file \
-        --device cuda --gpus 0 --run-name $run_name
+        --device cuda --gpus 0 --run-name $run_name \
+        --batch-size $batch_size --dataloader-workers $dataloader_workers
 else
     pytorch_config=${project_root}/experiments/${file_name}/train/pytorch_config.yaml
     echo "Debug mode is OFF, using default pytorch_config: $pytorch_config"
@@ -37,5 +42,6 @@ else
         $([ "$train_pose" -eq 1 ] && echo "--train-pose") \
         --project_root $data_root --pytorch_config $pytorch_config \
         --train_file $train_json --test_file $test_json \
-        --device cuda --gpus 0 --run-name $run_name
+        --device cuda --gpus 0 --run-name $run_name \
+        --batch-size $batch_size --dataloader-workers $dataloader_workers
 fi
