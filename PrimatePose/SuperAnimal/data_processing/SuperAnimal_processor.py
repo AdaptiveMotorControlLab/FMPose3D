6f1dddff26b3f2feae05e95cdf657172e234377a
ti <tiwang.cs@gmail.com>
2025-05-14 16:53:47 +0200

    add leave_one_dataset_out method  for dataset splitting

--- PrimatePose/SuperAnimal/data_processing/SuperAnimal_processor.py
+++ PrimatePose/SuperAnimal/data_processing/SuperAnimal_processor.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 class SuperAnimalProcessor:
     
@@ -18,6 +19,67 @@ class SuperAnimalProcessor:
         for dataset_name in sorted(unique_datasets):
             print(dataset_name)
 
+
+    @staticmethod
+    def leave_one_dataset_out(full_dataset_json_path, OOD_dataset_name, output_folder):
+        with open(full_dataset_json_path, 'r') as f:
+            data = json.load(f)
+        
+        images = data["images"]
+        annotations = data["annotations"]
+        
+        # Get mode from filename
+        mode = full_dataset_json_path.split("/")[-1].split(".")[0]
+        
+        # Split images
+        OOD_images = []
+        IID_images = []
+        OOD_image_ids = set()
+        IID_image_ids = set()
+        
+        for image in images:
+            if image["source_dataset"] == OOD_dataset_name:
+                OOD_images.append(image)
+                OOD_image_ids.add(image["id"])
+            else:
+                IID_images.append(image)
+                IID_image_ids.add(image["id"])
+        
+        # Split annotations
+        OOD_annotations = []
+        IID_annotations = []
+        
+        for annotation in annotations:
+            if annotation["image_id"] in OOD_image_ids:
+                OOD_annotations.append(annotation)
+            elif annotation["image_id"] in IID_image_ids:
+                IID_annotations.append(annotation)
+        
+        # Create output JSONs
+        OOD_data = {
+            "images": OOD_images,
+            "annotations": OOD_annotations,
+            "categories": data["categories"]
+        }
+        
+        IID_data = {
+            "images": IID_images,
+            "annotations": IID_annotations,
+            "categories": data["categories"]
+        }
+        
+        # Save output files
+        os.makedirs(output_folder, exist_ok=True)
+        
+        ood_filename = f"{mode}_OOD_{OOD_dataset_name}.json"
+        iid_filename = f"{mode}_IID_wo_{OOD_dataset_name}.json"
+        
+        with open(os.path.join(output_folder, ood_filename), 'w') as f:
+            json.dump(OOD_data, f)
+        
+        with open(os.path.join(output_folder, iid_filename), 'w') as f:
+            json.dump(IID_data, f)
+            
 
 if __name__ == "__main__":
     
