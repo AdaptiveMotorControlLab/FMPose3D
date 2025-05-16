0bda359e630771c57567c8ce3221ed3c7d3287af
ti <tiwang.cs@gmail.com>
2025-05-16 11:48:35 +0200

    Update training script for RTMPose with new dataset configuration and Weights & Biases integration

--- PrimatePose/coco_necessary_split/train_rtmpose_subdatasets.sh
+++ PrimatePose/coco_necessary_split/train_rtmpose_subdatasets.sh
@@ -5,17 +5,25 @@ data_root=${data_path_prefix}"/v8_coco"
 debug=0
 gpu_id="1"
 
-dataset_name=mbw
+dataset_name=lote
 file_name=${dataset_name}_pose_rtmpose_s_V82_20250516
 
 train_pose=1
 mode="train"
-# Generate run name based on configuration
-run_name="${file_name}"
 
 # for splitted datasets V8.2
 train_json="${data_path_prefix}/primate_data/PFM_V8.2/splitted_${mode}_datasets/${dataset_name}_${mode}.json"
 test_json="${data_path_prefix}/primate_data/PFM_V8.2/splitted_test_datasets/${dataset_name}_test.json"
+
+# Pose model training configuration
+batch_size=32
+dataloader_workers=4
+
+# wandb configuration
+wandb_project_name="primatepose"
+wandb_run_name="${file_name}"
+wandb_group="RTMPose_split_datasets_v82"
+wandb_tag="train"
 
 if [ "$debug" -eq 1 ]; then
     pytorch_config=${project_root}/project/Debug/${file}_${mode}/train/pytorch_config.yaml
@@ -41,5 +49,10 @@ else
         $([ "$train_pose" -eq 1 ] && echo "--train-pose") \
         --project_root $data_root --pytorch_config $pytorch_config \
         --train_file $train_json --test_file $test_json \
-        --device cuda --gpus 0 --run-name $run_name
+        --device cuda --gpus 0 \
+        --batch-size $batch_size --dataloader-workers $dataloader_workers \
+        --wandb-project-name $wandb_project_name \
+        --wandb-run-name $wandb_run_name \
+        --wandb-group $wandb_group \
+        --wandb-tag $wandb_tag
 fi
