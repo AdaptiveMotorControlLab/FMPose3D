d5edfb0b16e4f88be34070b0983e4899080f8509
ti <tiwang.cs@gmail.com>
2025-05-16 10:54:24 +0200

    Add script to train RTMPose with PFM splitted datasets

---
+++ PrimatePose/coco_necessary_split/train_rtmpose_subdatasets.sh
@@ -0,0 +1,45 @@
+project_root=$(dirname $(dirname $(realpath $0)))
+data_path_prefix="/home/ti_wang/data/tiwang"
+data_root=${data_path_prefix}"/v8_coco"
+
+debug=0
+gpu_id="1"
+
+dataset_name=mbw
+file_name=${dataset_name}_pose_rtmpose_s_V82_20250516
+
+train_pose=1
+mode="train"
+# Generate run name based on configuration
+run_name="${file_name}"
+
+# for splitted datasets V8.2
+train_json="${data_path_prefix}/primate_data/PFM_V8.2/splitted_${mode}_datasets/${dataset_name}_${mode}.json"
+test_json="${data_path_prefix}/primate_data/PFM_V8.2/splitted_test_datasets/${dataset_name}_test.json"
+
+if [ "$debug" -eq 1 ]; then
+    pytorch_config=${project_root}/project/Debug/${file}_${mode}/train/pytorch_config.yaml
+    echo "Debug mode is ON, using debug pytorch_config: $pytorch_config"
+    CUDA_VISIBLE_DEVICES=$gpu_id python3 train.py --debug \
+        $([ "$train_detector" -eq 1 ] && echo "--train-detector") \
+        $([ "$train_pose" -eq 1 ] && echo "--train-pose") \
+        --project_root $data_root --pytorch_config $pytorch_config \
+        --train_file $train_file --test_file $test_file \
+        --device cuda --gpus 0 --run-name $run_name
+else
+    pytorch_config=${project_root}/experiments/pfm_rtmpose/split/${file_name}/train/pytorch_config.yaml
+    echo "Debug mode is OFF, using default pytorch_config: $pytorch_config"
+    experiment_dir=${project_root}/experiments/pfm_rtmpose/split/${file_name}
+    echo "Copying training scripts to ${experiment_dir}"
+    cp "$0" "$experiment_dir/"
+    cp "${project_root}/coco_necessary_split/train.py" "$experiment_dir/"
+    cp "${project_root}/coco_necessary_split/create_coco_project_rtmpose_subdatasets.sh" "$experiment_dir/"
+    cp "${project_root}/coco_necessary_split/make_config.py" "$experiment_dir/"
+
+    CUDA_VISIBLE_DEVICES=$gpu_id python train.py \
+        $([ "$train_detector" -eq 1 ] && echo "--train-detector") \
+        $([ "$train_pose" -eq 1 ] && echo "--train-pose") \
+        --project_root $data_root --pytorch_config $pytorch_config \
+        --train_file $train_json --test_file $test_json \
+        --device cuda --gpus 0 --run-name $run_name
+fi
