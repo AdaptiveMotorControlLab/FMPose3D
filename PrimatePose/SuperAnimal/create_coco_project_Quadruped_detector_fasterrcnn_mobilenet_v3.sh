a1a0ecb5308a30454c0aab3ee056d167a9de7d85
ti <tiwang.cs@gmail.com>
2025-06-16 14:27:19 +0200

    update file names and adjust JSON paths with merged dataset

--- PrimatePose/SuperAnimal/create_coco_project_Quadruped_detector_fasterrcnn_mobilenet_v3.sh
+++ PrimatePose/SuperAnimal/create_coco_project_Quadruped_detector_fasterrcnn_mobilenet_v3.sh
@@ -5,7 +5,7 @@ project_root=$(dirname $(realpath $0))
 debug=0
 OOD_dataset_name=AP-10K
 
-file_name=SAQ_detector_fasterrcnn_mobilenet_v3_OOD_${OOD_dataset_name}_20250613
+file_name=SAQ_detector_fasterrcnn_mobilenet_v3_allData_${OOD_dataset_name}_20250616
 
 mode="train"
 
@@ -13,7 +13,7 @@ mode="train"
 # train_json="${data_root}/annotations/${mode}_IID_wo_${OOD_dataset_name}.json"
 # test_json="${data_root}/annotations/test_IID_wo_${OOD_dataset_name}.json"
 
-train_json="${data_root}/annotations/merged_train_and_test_wo_AP-10K.json"
+train_json="${data_root}/annotations/merged_train_and_test.json"
 test_json="${data_root}/annotations/test_OOD_${OOD_dataset_name}.json"
 
 model_arch="rtmpose_s"
