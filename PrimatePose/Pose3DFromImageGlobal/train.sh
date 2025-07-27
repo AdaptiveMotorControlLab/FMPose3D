25f0d4adc4eabdab3d8f7ddc0886d09151a1e866
ti <tiwang.cs@gmail.com>
2025-07-27 16:21:46 +0200

    train.sh

---
+++ PrimatePose/Pose3DFromImageGlobal/train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Training script for 3D Pose Estimation (Global coordinate system)
+# This script automatically copies source files and saves parameters for reproducibility
+
+NVIDIA_VISIBLE_DEVICES=1 python train.py \
+    --dataset_name "oms" \
+    --train_json /home/ti_wang/Ti_workspace/PrimatePose/Pose3D/data/oms_train.json \
+    --val_json /home/ti_wang/Ti_workspace/PrimatePose/Pose3D/data/oms_test.json \
+    --epochs 200 \
+    --batch_size 128 \
+    --lr 5e-4 \
+    --gpu 1 \
+    --max_checkpoints 5 \
+    --save_freq 5 \
+    --seed 1
