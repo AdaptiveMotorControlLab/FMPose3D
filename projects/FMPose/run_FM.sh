1d493e8437c5a29b40c3741e87241f9163930701
ti <tiwang.cs@gmail.com>
2025-09-10 23:54:35 +0200

    Uncomment training command in run_FM.sh and add test commands for CFM. Enhance script usability for model training and testing.

--- projects/FMPose/run_FM.sh
+++ projects/FMPose/run_FM.sh
@@ -1,4 +1,5 @@
 #Train CFM
-# python3 main_CFM.py --train --model model_GUMLP --layers 4 --lr 1e-3 --lr_decay 0.98 --nepoch 100 --sample_steps 3 --gpu 0 --debug
+python3 main_CFM.py --train --model model_GUMLP --layers 4 --lr 1e-3 --lr_decay 0.98 --nepoch 100 --sample_steps 3 --gpu 0 --debug
 #Test CFM
-python3 main_CFM.py --reload --previous_dir "./debug/250906_2235_46" --model model_GUMLP --sample_steps 3 --test_augmentation True --layers 4 --gpu 0
+# python3 main_CFM.py --reload --previous_dir "./debug/250908_1418_45" --model model_GUMLP --sample_steps 3 --test_augmentation True --layers 5 --gpu 0
+# python3 main_CFM_test_vis.py --reload --previous_dir "./debug/250906_2235_46" --model model_GUMLP --sample_steps 19 --batch_size 1 --test_augmentation True --layers 4 --gpu 0
