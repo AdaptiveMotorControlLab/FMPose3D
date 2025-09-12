64014523f018b63260a4136d8abcec44a0b368af
ti <tiwang.cs@gmail.com>
2025-09-12 21:48:27 +0200

    Refactor run_FM_debug.sh to improve model configuration handling. Introduce variables for layers, learning rate, decay, sample steps, and GPU ID for better readability and flexibility in training command. Update folder name generation to reflect new model parameters.

--- projects/FMPose/run_FM_debug.sh
+++ projects/FMPose/run_FM_debug.sh
@@ -1,5 +1,11 @@
 #Train CFM
-folder_name=FM_attention_x0_noise_layers6_lr1e-3_decay0.98_sample3_$(date +%Y%m%d_%H%M)
+layers=5
+lr=1e-3
+decay=0.98
+sample_steps=3
+gpu_id=3
+model_name=model_GraphAttention
+folder_name=FM_GraphAttention_x0_noise_layers${layers}_lr${lr}_decay${decay}_sample${sample_steps}_$(date +%Y%m%d_%H%M)
 
 # Read WANDB_API_KEY from file if not provided via env
 key_file="$(dirname "$0")/wandb_api_key.txt"
@@ -10,7 +16,7 @@ fi
 if [ -n "$WANDB_API_KEY" ]; then
   wandb login --relogin "$WANDB_API_KEY" >/dev/null 2>&1 || true
 fi
-python3 main_CFM.py --train --model model_attention --gpu 0 --layers 6 --lr 1e-3 --lr_decay 0.98 --nepoch 100 --sample_steps 3 --folder_name $folder_name --debug
+python3 main_CFM.py --train --model ${model_name} --gpu ${gpu_id} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch 100 --sample_steps ${sample_steps} --folder_name $folder_name --debug
 #Test CFM
 # python3 main_CFM.py --reload --previous_dir "./debug/250908_1418_45" --model model_GUMLP --sample_steps 3 --test_augmentation True --layers 5 --gpu 0
 # python3 main_CFM_test_vis.py --reload --previous_dir "./debug/250906_2235_46" --model model_GUMLP --sample_steps 19 --batch_size 1 --test_augmentation True --layers 4 --gpu 0
