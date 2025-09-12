04398de4f2e81723ea127e67ff1b2fc79d839ae6
ti <tiwang.cs@gmail.com>
2025-09-12 21:48:37 +0200

    Update run_FM.sh to support CrossAttention model training. Introduce variables for layers, learning rate, decay, and GPU ID for improved configuration. Modify folder name generation to reflect new model parameters and update training command for enhanced evaluation capabilities.

--- projects/FMPose/run_FM.sh
+++ projects/FMPose/run_FM.sh
@@ -1,5 +1,10 @@
 #Train CFM
-folder_name=FM_x0_noise_layers4_lr1e-3_decay0.98_sample3_$(date +%Y%m%d_%H%M)
+layers=10
+lr=1e-3
+decay=0.98
+gpu_id=1
+model_name=model_CrossAttention
+folder_name=FM_CrossAttention_x0_T_pose_noise_layers${layers}_lr${lr}_decay${decay}_$(date +%Y%m%d_%H%M)
 
 # Read WANDB_API_KEY from file if not provided via env
 key_file="$(dirname "$0")/wandb_api_key.txt"
@@ -10,7 +15,7 @@ fi
 if [ -n "$WANDB_API_KEY" ]; then
   wandb login --relogin "$WANDB_API_KEY" >/dev/null 2>&1 || true
 fi
-python3 main_CFM.py --train --model model_GUMLP --gpu 1 --layers 4 --lr 1e-3 --lr_decay 0.98 --nepoch 100 --sample_steps 3 --folder_name $folder_name
+python3 main_CFM_T_pose.py --train --model ${model_name} --gpu ${gpu_id} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch 100 --eval_multi_steps --eval_sample_steps 1,3,5,7,9 --folder_name $folder_name 
 #Test CFM
 # python3 main_CFM.py --reload --previous_dir "./debug/250908_1418_45" --model model_GUMLP --sample_steps 3 --test_augmentation True --layers 5 --gpu 0
 # python3 main_CFM_test_vis.py --reload --previous_dir "./debug/250906_2235_46" --model model_GUMLP --sample_steps 19 --batch_size 1 --test_augmentation True --layers 4 --gpu 0
