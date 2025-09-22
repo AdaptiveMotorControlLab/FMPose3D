9714b23065161acf115169cd10ceecc1136299ef
ti <tiwang.cs@gmail.com>
2025-09-22 23:33:59 +0200

    Update run_FM.sh to modify training parameters:

--- projects/FMPose/run_FM.sh
+++ projects/FMPose/run_FM.sh
@@ -1,16 +1,16 @@
 #Train CFM
-layers=4
+layers=5
 lr=1e-3
 decay=0.95
 gpu_id=0
-eval_multi_steps=3
+eval_sample_steps=3
 batch_size=256
 large_decay_epoch=5
-lr_decay_large=0.8
-epochs=100
+lr_decay_large=0.9
+epochs=50
 num_saved_models=5
-model_name=model_GAMLP
-folder_name=FM_GAMLP_noisePose_layers${layers}_1GCNParallelAttnMLP_attnD_0.1_projD_0.2_lr${lr}_decay${decay}_lr_decay_large_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M)
+model_name=model_G_P_Attn
+folder_name=FM_GAMLP_noisePose_1GCN_P_Attn_LearnableWeightsG0.5_A0.5_attnD_0.2_projD_0.2_layers${layers}_lr${lr}_decay${decay}_lr_decay_large_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
 
 # Read WANDB_API_KEY from file if not provided via env
 key_file="$(dirname "$0")/wandb_api_key.txt"
@@ -21,9 +21,9 @@ fi
 if [ -n "$WANDB_API_KEY" ]; then
   wandb login --relogin "$WANDB_API_KEY" >/dev/null 2>&1 || true
 fi
-
 # python3 main_CFM_noise_pose.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_multi_steps --eval_sample_steps ${eval_multi_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large}
-python3 main_CFM_noise_pose.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_multi_steps --eval_sample_steps ${eval_multi_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models}
+# python3 main_CFM_noise_pose.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models}
+python3 main_CFM_noise_pose_weights_GA.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models}
 
 #Test CFM
 # python3 main_CFM.py --reload --previous_dir "./debug/250908_1418_45" --model model_GUMLP --sample_steps 3 --test_augmentation True --layers 5 --gpu 0
