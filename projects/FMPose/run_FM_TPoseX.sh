ea32fba682a12b2023587c41f542b03370960be7
ti <tiwang.cs@gmail.com>
2025-09-24 20:51:47 +0200

    Update run_FM_TPoseX.sh to include dynamic script name parameter: Added a new variable for the script name to enhance execution context and ensure proper handling of the script during training.

--- projects/FMPose/run_FM_TPoseX.sh
+++ projects/FMPose/run_FM_TPoseX.sh
@@ -12,6 +12,7 @@ num_saved_models=5
 model_path='model/model_G_P_Attn.py'
 folder_name=FM_GAMLP_TPose_1GCN_P_Attn_attnD_0.2_projD_0.25_layers${layers}_lr${lr}_Decay${decay}_lrDLarge_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
 
+sh_file='run_FM_TPoseX.sh'
 # Read WANDB_API_KEY from file if not provided via env
 key_file="$(dirname "$0")/wandb_api_key.txt"
 if [ -z "$WANDB_API_KEY" ] && [ -f "$key_file" ]; then
@@ -22,7 +23,7 @@ if [ -n "$WANDB_API_KEY" ]; then
   wandb login --relogin "$WANDB_API_KEY" >/dev/null 2>&1 || true
 fi
 # python3 main_CFM_noise_pose.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_multi_steps --eval_sample_steps ${eval_multi_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large}
-python3 main_CFM_T_pose.py --train --model_path ${model_path} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models}
+python3 main_CFM_T_pose.py --train --model_path ${model_path} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models} --sh_file ${sh_file}
 # python3 main_CFM_noise_pose_weights_GA.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models}
 
 #Test CFM
