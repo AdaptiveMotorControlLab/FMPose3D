30689719dfb24a1620e2cd81f35c8a1f528b2d5c
ti <tiwang.cs@gmail.com>
2025-09-25 20:35:26 +0200

    update

--- projects/FMPose/run_FM_TPoseX.sh
+++ projects/FMPose/run_FM_TPoseX.sh
@@ -2,7 +2,7 @@
 layers=5
 lr=1e-3
 decay=0.95
-gpu_id=1
+gpu_id=0
 eval_sample_steps=3
 batch_size=256
 large_decay_epoch=5
@@ -10,7 +10,7 @@ lr_decay_large=0.8
 epochs=60
 num_saved_models=5
 model_path='model/model_G_P_Attn.py'
-folder_name=FM_GAMLP_TPose_1GCN_P_Attn_attnD_0.2_projD_0.25_layers${layers}_lr${lr}_Decay${decay}_lrDLarge_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
+folder_name=FM_GAMLP_TPose_noNoise_1GCN_P_Attn_attnD_0.2_projD_0.25_layers${layers}_lr${lr}_Decay${decay}_lrDLarge_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
 
 sh_file='run_FM_TPoseX.sh'
 # Read WANDB_API_KEY from file if not provided via env
@@ -23,8 +23,7 @@ if [ -n "$WANDB_API_KEY" ]; then
   wandb login --relogin "$WANDB_API_KEY" >/dev/null 2>&1 || true
 fi
 # python3 main_CFM_noise_pose.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_multi_steps --eval_sample_steps ${eval_multi_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large}
-python3 main_CFM_T_pose.py --train --model_path ${model_path} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models} --sh_file ${sh_file}
-# python3 main_CFM_noise_pose_weights_GA.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models}
+python3 main_CFM_TPose.py --train --model_path ${model_path} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models} --sh_file ${sh_file}
 
 #Test CFM
 # python3 main_CFM.py --reload --previous_dir "./debug/250908_1418_45" --model model_GUMLP --sample_steps 3 --test_augmentation True --layers 5 --gpu 0
