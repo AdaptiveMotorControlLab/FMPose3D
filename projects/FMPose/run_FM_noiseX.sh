a3005313b25e8d72956c8fe202b18c12968ee375
ti <tiwang.cs@gmail.com>
2025-09-25 20:35:12 +0200

    update

--- projects/FMPose/run_FM_noiseX.sh
+++ projects/FMPose/run_FM_noiseX.sh
@@ -2,15 +2,14 @@
 layers=5
 lr=1e-3
 decay=0.95
-gpu_id=0
-eval_sample_steps=3
+gpu_id=1
+eval_sample_steps=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
 batch_size=256
 large_decay_epoch=5
 lr_decay_large=0.9
 epochs=50
 num_saved_models=5
 model_path='model/model_G_P_Attn.py'
-folder_name=FM_GAMLP_noisePose_1GCN_P_Attn_attnD_0.2_projD_0.25_layers${layers}_lr${lr}_decay${decay}_lr_decay_large_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
 
 sh_file='run_FM_noiseX.sh'
 # Read WANDB_API_KEY from file if not provided via env
@@ -23,27 +22,26 @@ if [ -n "$WANDB_API_KEY" ]; then
   wandb login --relogin "$WANDB_API_KEY" >/dev/null 2>&1 || true
 fi
 
-# python3 main_CFM_noise_pose.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_multi_steps --eval_sample_steps ${eval_multi_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large}
-python3 main_CFM_noise_pose.py --train --model_path ${model_path} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models} --sh_file ${sh_file}
-# python3 main_CFM_noise_pose_weights_GA.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models}
+# python3 main_CFM_noise_pose.py --train --model_path ${model_path} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models} --sh_file ${sh_file}
 
+# python3 main_CFM_noise_pose_weights_GA.py --train --model_path ${model_path} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models}
 
-#Test CFM
-# python3 main_CFM_noise_pose.py \
-# --reload \
-# --saved_model_path "checkpoint/FM_GAMLP_noisePose_layers5_1GCNParallelAttnMLP_attnD_0.2_projD_0.25_lr1e-3_decay0.98_lr_decay_large_e5_0.8_B256_20250916_1953/CFM_36_4972_best.pth" \
-# --model ${model_name} \
-# --gpu ${gpu_id} \
-# --batch_size ${batch_size} \
-# --layers ${layers} \
-# --lr ${lr} \
-# --lr_decay ${decay} \
-# --nepoch ${epochs} \
-# --eval_sample_steps ${eval_sample_steps} \
-# --folder_name $folder_name \
-# --large_decay_epoch ${large_decay_epoch} \
-# --lr_decay_large ${lr_decay_large} \
-# --num_saved_models ${num_saved_models}
+# Testing CFM
+model_path='./pretrained_model/FM_GAMLP_noisePose_layers5_1GCNParallelAttnMLP_attnD_0.2_projD_0.25_lr1e-3_decay0.98_lr_decay_large_e5_0.8_B256_20250916_1953/250916_1953_32_model_GAMLP.py'
+saved_model_path='./pretrained_model/FM_GAMLP_noisePose_layers5_1GCNParallelAttnMLP_attnD_0.2_projD_0.25_lr1e-3_decay0.98_lr_decay_large_e5_0.8_B256_20250916_1953/CFM_36_4972_best.pth'
+
+python3 main_CFM_noise_pose_test.py \
+--reload \
+--saved_model_path ${saved_model_path} \
+--model_path ${model_path} \
+--gpu ${gpu_id} \
+--batch_size ${batch_size} \
+--layers ${layers} \
+--nepoch ${epochs} \
+--eval_sample_steps ${eval_sample_steps} \
+--num_saved_models ${num_saved_models} \
+--sh_file ${sh_file}
+
 #Test CFM
 # python3 main_CFM.py --reload --previous_dir "./debug/250908_1418_45" --model model_GUMLP --sample_steps 3 --test_augmentation True --layers 5 --gpu 0
 # python3 main_CFM.py --reload --previous_dir "./checkpoint/FM_x0_noise_layers4_lr1e-3_decay0.98_sample3_20250911" --model ${model_name} --sample_steps 3 --test_augmentation True --batch_size ${batch_size} --layers ${layers} --gpu ${gpu_id}
