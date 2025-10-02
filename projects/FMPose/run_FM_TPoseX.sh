05c8e7076fde28ddf7592b1a8dd303d1d0c122ae
ti <tiwang.cs@gmail.com>
2025-10-02 19:11:18 +0200

    Update training script parameters: increase decay rate to 0.98 and adjust evaluation sample steps to 1. Modify folder naming convention for clarity. Include test augmentation flag in training command.

--- projects/FMPose/run_FM_TPoseX.sh
+++ projects/FMPose/run_FM_TPoseX.sh
@@ -1,16 +1,16 @@
 #Train CFM
 layers=5
 lr=1e-3
-decay=0.95
+decay=0.98
 gpu_id=0
-eval_sample_steps=3
+eval_sample_steps=1
 batch_size=256
 large_decay_epoch=5
 lr_decay_large=0.8
 epochs=60
 num_saved_models=5
 model_path='model/model_G_P_Attn.py'
-folder_name=FM_GAMLP_TPose_noNoise_1GCN_P_Attn_attnD_0.2_projD_0.25_layers${layers}_lr${lr}_Decay${decay}_lrDLarge_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
+folder_name=FM_GAMLP_TPose_noNoise_GT0_1GCN_P_Attn_attnD_0.2_projD_0.25_layers${layers}_lr${lr}_Decay${decay}_lrDLarge_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
 
 sh_file='run_FM_TPoseX.sh'
 # Read WANDB_API_KEY from file if not provided via env
@@ -23,7 +23,7 @@ if [ -n "$WANDB_API_KEY" ]; then
   wandb login --relogin "$WANDB_API_KEY" >/dev/null 2>&1 || true
 fi
 # python3 main_CFM_noise_pose.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_multi_steps --eval_sample_steps ${eval_multi_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large}
-python3 main_CFM_TPose.py --train --model_path ${model_path} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models} --sh_file ${sh_file}
+python3 main_CFM_TPose.py --train --test_augmentation False --model_path ${model_path} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_sample_steps ${eval_sample_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large} --num_saved_models ${num_saved_models} --sh_file ${sh_file}
 
 #Test CFM
 # python3 main_CFM.py --reload --previous_dir "./debug/250908_1418_45" --model model_GUMLP --sample_steps 3 --test_augmentation True --layers 5 --gpu 0
