7482592b3b933b984a125f639ce2f30609676154
ti <tiwang.cs@gmail.com>
2025-10-02 19:13:37 +0200

    Update `run_FM_multi_hypothesis.sh` script: increase batch size to 1024, expand `num_hypothesis_list` to include values from 1 to 20, and adjust evaluation steps to 3. Introduce new parameters for model path, saved model path, and additional configurations for testing. Update the command to run the multi-hypothesis test with new arguments for top-k and weight softmax tau.

--- projects/FMPose/run_FM_multi_hypothesis.sh
+++ projects/FMPose/run_FM_multi_hypothesis.sh
@@ -2,17 +2,14 @@
 layers=5
 lr=1e-3
 decay=0.95
-gpu_id=0
-eval_multi_steps=1
-batch_size=64
+batch_size=1024
 large_decay_epoch=5
 lr_decay_large=0.9
 epochs=100
 # Pass multiple values correctly to argparse using an array
-num_hypothesis_list=1,2
+# num_hypothesis_list=1,2,3,4,5,6,7,8,9,10
 
 model_name=model_GUMLP
-folder_name=FM_GMLP_noisePose_layers${layers}_1GCN1MLP_lr${lr}_decay${decay}_lr_decay_large_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M)
 
 sh_file='run_FM_multi_hypothesis.sh'
 
@@ -25,14 +22,25 @@ fi
 if [ -n "$WANDB_API_KEY" ]; then
   wandb login --relogin "$WANDB_API_KEY" >/dev/null 2>&1 || true
 fi
-# python3 main_CFM_noise_pose.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_multi_steps --eval_sample_steps ${eval_multi_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large}
 
-model_path='checkpoint/FM_GAMLP_noisePose_layers5_1GCNParallelAttnMLP_attnD_0.2_projD_0.25_lr1e-3_decay0.98_lr_decay_large_e5_0.8_B256_20250916_1953/250916_1953_32_model_GAMLP.py'
-saved_model_path='checkpoint/FM_GAMLP_noisePose_layers5_1GCNParallelAttnMLP_attnD_0.2_projD_0.25_lr1e-3_decay0.98_lr_decay_large_e5_0.8_B256_20250916_1953/CFM_36_4972_best.pth'
+weight_softmax_tau=1.0
+num_hypothesis_list=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+eval_multi_steps=3
+topk=3
+gpu_id=0
+mode='exp'
+temp=0.001
+# python3 main_CFM_noise_pose.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_multi_steps --eval_sample_steps ${eval_multi_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large}
+folder_name=CamW_aug_s${eval_multi_steps}h${num_hypothesis_list}_Tau${tau}_Top${topk}_${mode}_temp${temp}_test_results_$(date +%Y%m%d_%H%M%S)
+model_path='pretrained_model/FM_GAMLP_noisePose_layers5_1GCNParallelAttnMLP_attnD_0.2_projD_0.25_lr1e-3_decay0.98_lr_decay_large_e5_0.8_B256_20250916_1953/250916_1953_32_model_GAMLP.py'
+saved_model_path='pretrained_model/FM_GAMLP_noisePose_layers5_1GCNParallelAttnMLP_attnD_0.2_projD_0.25_lr1e-3_decay0.98_lr_decay_large_e5_0.8_B256_20250916_1953/CFM_36_4972_best.pth'
 
 #Test CFM
-python3 main_CFM_noise_pose_multiHypothesis.py \
+python3 main_CFM_noise_pose_multiHypothesis_test.py \
 --reload \
+--topk ${topk} \
+--weight_softmax_tau ${weight_softmax_tau} \
+--folder_name ${folder_name} \
 --saved_model_path "${saved_model_path}" \
 --model_path "${model_path}" \
 --eval_sample_steps ${eval_multi_steps} \
