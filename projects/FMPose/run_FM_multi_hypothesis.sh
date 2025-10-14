a5054331c4cbe70e6c02c02311f6938bfddea60e
ti <tiwang.cs@gmail.com>
2025-10-14 17:06:21 +0200

    Update parameters for multi-hypothesis testing

--- projects/FMPose/run_FM_multi_hypothesis.sh
+++ projects/FMPose/run_FM_multi_hypothesis.sh
@@ -26,12 +26,12 @@ fi
 weight_softmax_tau=1.0
 num_hypothesis_list=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
 eval_multi_steps=3
-topk=6
-gpu_id=1
+topk=10
+gpu_id=0
 mode='exp'
-exp_temp=0.005
+exp_temp=0.01
 # python3 main_CFM_noise_pose.py --train --model ${model_name} --gpu ${gpu_id} --batch_size ${batch_size} --layers ${layers} --lr ${lr} --lr_decay ${decay} --nepoch ${epochs} --eval_multi_steps --eval_sample_steps ${eval_multi_steps} --folder_name $folder_name --large_decay_epoch ${large_decay_epoch} --lr_decay_large ${lr_decay_large}
-folder_name=exp_FlipHAug_noflipforNoise_s${eval_multi_steps}_Top${topk}_${mode}_temp${exp_temp}_h${num_hypothesis_list}_test_results_$(date +%Y%m%d_%H%M%S)
+folder_name=exp_FlipHAug_s${eval_multi_steps}_Top${topk}_${mode}_temp${exp_temp}_h${num_hypothesis_list}_noflipforNoise_test_results_$(date +%Y%m%d_%H%M%S)
 model_path='pretrained_model/FM_GAMLP_noisePose_layers5_1GCNParallelAttnMLP_attnD_0.2_projD_0.25_lr1e-3_decay0.98_lr_decay_large_e5_0.8_B256_20250916_1953/250916_1953_32_model_GAMLP.py'
 saved_model_path='pretrained_model/FM_GAMLP_noisePose_layers5_1GCNParallelAttnMLP_attnD_0.2_projD_0.25_lr1e-3_decay0.98_lr_decay_large_e5_0.8_B256_20250916_1953/CFM_36_4972_best.pth'
 
