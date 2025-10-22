4224dd14f285645d6e2e92f023af348a0be59ad5
ti <tiwang.cs@gmail.com>
2025-10-22 10:31:37 +0200

    delete wandb

--- projects/FMPose/run_FM_noiseX.sh
+++ projects/FMPose/run_FM_noiseX.sh
@@ -7,22 +7,13 @@ eval_sample_steps=3
 batch_size=256
 large_decay_epoch=5
 lr_decay_large=0.8
-epochs=100
+epochs=80
 num_saved_models=3
 frames=1
-model_path='model/model_G_P_Attn.py'
+channel_dim=512
+model_path='./model/model_attention.py'
 sh_file='run_FM_noiseX.sh'
-folder_name=FM_GPA_Noise_1GCN_P_Attn_layers${layers}_lr${lr}_decay${decay}_lr_decay_large_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
-
-# Read WANDB_API_KEY from file if not provided via env
-key_file="$(dirname "$0")/wandb_api_key.txt"
-if [ -z "$WANDB_API_KEY" ] && [ -f "$key_file" ]; then
-  WANDB_API_KEY="$(head -n1 "$key_file" | tr -d ' \n\r')"
-fi
-
-if [ -n "$WANDB_API_KEY" ]; then
-  wandb login --relogin "$WANDB_API_KEY" >/dev/null 2>&1 || true
-fi
+folder_name=FM_GPA_Noise_Ablaiton_Attn_A_layers${layers}_lr${lr}_decay${decay}_lr_decay_large_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
 
 #--keypoints gt \
 # training
@@ -42,7 +33,8 @@ python3 main_CFM_noise_pose.py \
   --large_decay_epoch ${large_decay_epoch} \
   --lr_decay_large ${lr_decay_large} \
   --num_saved_models ${num_saved_models} \
-  --sh_file ${sh_file}
+  --sh_file ${sh_file} \
+  --channel ${channel_dim}
 
 # Testing CFM
 # model_path='./pretrained_model/FM_GAMLP_noisePose_layers5_1GCNParallelAttnMLP_attnD_0.2_projD_0.25_lr1e-3_decay0.98_lr_decay_large_e5_0.8_B256_20250916_1953/250916_1953_32_model_GAMLP.py'
