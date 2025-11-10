1f93271546d1053c55e49a70c187e60f2d6061a2
ti <tiwang.cs@gmail.com>
2025-11-10 17:42:02 +0100

    Update model path and folder name in run_FM_noiseX.sh for testing configuration: Changed model reference to model_G_P_Attn.py and modified folder naming convention for clarity in test runs.

--- projects/FMPose/run_FM_noiseX.sh
+++ projects/FMPose/run_FM_noiseX.sh
@@ -11,9 +11,9 @@ epochs=80
 num_saved_models=3
 frames=1
 channel_dim=512
-model_path='./model/model_attention.py'
+model_path='./model/model_G_P_Attn.py'
 sh_file='run_FM_noiseX.sh'
-folder_name=FM_GPA_Noise_Ablaiton_Attn_A_layers${layers}_lr${lr}_decay${decay}_lr_decay_large_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
+folder_name=FM_GPA_TestP_deleteclone_inGCN_1GCN_P_Attn_layers${layers}_lr${lr}_decay${decay}_lr_decay_large_e${large_decay_epoch}_${lr_decay_large}_B${batch_size}_$(date +%Y%m%d_%H%M%S)
 
 #--keypoints gt \
 # training
@@ -51,8 +51,3 @@ python3 main_CFM_noise_pose.py \
 # --eval_sample_steps ${eval_sample_steps} \
 # --num_saved_models ${num_saved_models} \
 # --sh_file ${sh_file}
-
-#Test CFM
-# python3 main_CFM.py --reload --previous_dir "./debug/250908_1418_45" --model model_GUMLP --sample_steps 3 --test_augmentation True --layers 5 --gpu 0
-# python3 main_CFM.py --reload --previous_dir "./checkpoint/FM_x0_noise_layers4_lr1e-3_decay0.98_sample3_20250911" --model ${model_name} --sample_steps 3 --test_augmentation True --batch_size ${batch_size} --layers ${layers} --gpu ${gpu_id}
-# python3 main_CFM_T_pose.py --reload --previous_dir "./checkpoint/FM_GMLP_Tpose_noise_layers5_lr1e-3_decay0.95_B1024_20250913_1555" --model ${model_name} --eval_multi_steps --eval_sample_steps ${eval_multi_steps} --batch_size ${batch_size} --test_augmentation True --layers ${layers} --gpu ${gpu_id}
