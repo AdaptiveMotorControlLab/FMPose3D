2cda50c549537534dcabbcee0e3ed2ff12eb0af0
ti <tiwang.cs@gmail.com>
2025-11-29 14:59:31 +0100

    package

---
+++ fmpose/common/__init__.py
@@ -0,0 +1,32 @@
+"""
+Common utilities for FMPose.
+"""
+
+from .arguments import opts
+from .h36m_dataset import Human36mDataset
+from .load_data_hm36 import Fusion
+from .utils import (
+    mpjpe_cal,
+    p_mpjpe,
+    AccumLoss,
+    save_model,
+    save_top_N_models,
+    test_calculation,
+    print_error,
+    get_varialbe,
+)
+
+__all__ = [
+    "opts",
+    "Human36mDataset",
+    "Fusion",
+    "mpjpe_cal",
+    "p_mpjpe",
+    "AccumLoss",
+    "save_model",
+    "save_top_N_models",
+    "test_calculation",
+    "print_error",
+    "get_varialbe",
+]
+
