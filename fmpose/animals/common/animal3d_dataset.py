8180f8e569f63f2352014727e7dc41355fec7e0c
ti <tiwang.cs@gmail.com>
2025-12-18 18:09:35 +0100

    Remove unused animal3d_dataset_ti.py file and refactor TrainDataset in animal3d_dataset.py to streamline keypoint processing and enhance root-relative calculations.

--- fmpose/animals/common/animal3d_dataset.py
+++ fmpose/animals/common/animal3d_dataset.py
@@ -1,22 +1,15 @@
 import json
-from typing import List, Optional, Tuple
 
 import numpy as np
-import torch
-from common.camera import normalize_screen_coordinates
-from PIL import Image
-from torch.utils.data import ConcatDataset, DataLoader, Dataset
-
-# from yacs.config import CfgNode
-
-
-
+from .camera import normalize_screen_coordinates
+from torch.utils.data import Dataset
 
 
 class TrainDataset(Dataset):
-    def __init__(self, is_train: bool, json_file: str):
+    def __init__(self, is_train: bool, json_file: str, root_joint: int = 12):
         super().__init__()
         self.focal_length = 1000
+        self.root_joint = root_joint  # Root joint index for making coordinates relative
 
         json_file = json_file
         with open(json_file, "r") as f:
@@ -46,54 +39,19 @@ class TrainDataset(Dataset):
             keypoint_3d = np.concatenate((kp3d, np.ones((len(kp3d), 1), dtype=np.float32)), axis=-1)
         else:
             keypoint_3d = np.zeros((len(keypoint_2d), 4), dtype=np.float32)
+
+        # Make 3D keypoints root-relative
+        if keypoint_3d.shape[0] > self.root_joint:
+            root_pos = keypoint_3d[self.root_joint : self.root_joint + 1, :].copy()  # (1, 4)
+            keypoint_3d = keypoint_3d - root_pos  # All joints relative to root
+            # Now root joint should be exactly [0,0,0,0]
+
         bbox = data["bbox"]  # [x, y, w, h]
         ori_keypoint_2d = keypoint_2d.copy()
-        # print("keypoint_3d:",keypoint_3d.shape)
-        # root-relative to joint 0
-        if keypoint_3d.shape[0] > 1:
-            keypoint_3d[1:, :] -= keypoint_3d[:1, :]
 
         item = {
             "keypoints_2d": keypoint_2d,  #
             "keypoints_3d": keypoint_3d,
-        }
-        return item
-
-
-class EvaluationDataset(Dataset):
-    def __init__(self, json_file: str):
-        super().__init__()
-        self.focal_length = 1000
-
-        with open(json_file, "r") as f:
-            self.data = json.load(f)
-
-    def __len__(self):
-        return len(self.data["data"])
-
-    def __getitem__(self, item):
-        data = self.data["data"][item]
-        # read 2D keypoints (switch to reproj if desired)
-        keypoint_2d = np.array(data.get("keypoint_2d", []), dtype=np.float32)
-
-        # build 3D keypoints and root-relative to joint 0
-        if "keypoint_3d" in data and data["keypoint_3d"] is not None:
-            keypoint_3d = np.concatenate(
-                (data["keypoint_3d"], np.ones((len(data["keypoint_3d"]), 1))), axis=-1
-            ).astype(np.float32)
-            keypoint_3d[:, 1:, :] -= keypoint_3d[:, :1, :]
-        else:
-            keypoint_3d = np.zeros((len(keypoint_2d), 4), dtype=np.float32)
-        bbox = data["bbox"]  # [x, y, w, h]
-        # normalize 2D keypoints
-        hight = np.array(data["height"], dtype=np.float32)
-        width = np.array(data["width"], dtype=np.float32)
-        keypoint_2d = normalize_screen_coordinates(keypoint_2d[..., :2], width, hight)
-
-        ori_keypoint_2d = keypoint_2d.copy()
-
-        item = {
-            "keypoints_2d": keypoint_2d,
-            "keypoints_3d": keypoint_3d,
+            "img_path": str(data["img_path"]),
         }
         return item
