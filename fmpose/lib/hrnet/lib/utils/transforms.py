22876ad3531ced7d92b3bb5c5a0f8d98ce37e856
ti <tiwang.cs@gmail.com>
2026-01-18 13:30:22 +0100

    Translate comment in transforms.py from Chinese to English

--- fmpose/lib/hrnet/lib/utils/transforms.py
+++ fmpose/lib/hrnet/lib/utils/transforms.py
@@ -21,7 +21,7 @@ def flip_back(output_flipped, matched_parts):
 
     output_flipped = output_flipped[:, :, :, ::-1]
 
-    # 因为你输入的是翻转后的图像，所以输出的热图他们对应的左右关节也是相反的（训练的时候，输入的是翻转后的图像，target对应的左右关节也是对调过来的）。
+    # Input is a flipped image, so the output heatmaps need left/right joints swapped.
     for pair in matched_parts:
         tmp = output_flipped[:, pair[0], :, :].copy()
         output_flipped[:, pair[0], :, :] = output_flipped[:, pair[1], :, :]
